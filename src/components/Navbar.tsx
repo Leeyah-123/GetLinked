24e9e83e5f961bf38b41d37b81a7d8bf8615a53a
Leeyah-123 <junaidaaliyah260@gmail.com>
2023-09-19 23:32:28 +0100

    Fix register button inverse scaling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,9 @@ import Logo from './Logo';
 const Navbar = () => {
   return (
     <nav className="py-12 border-b border-b-slate-400 px-5 md:px-10 lg:px-20 flex items-center">
-      <Logo />
+      <Link to="/">
+        <Logo />
+      </Link>
 
       <div className="ml-auto">
         <div className="space-x-32 hidden md:flex items-center">
@@ -26,7 +28,7 @@ const Navbar = () => {
           <Link
             id="navBtn"
             to="/register"
-            className="btn transition-transform hover:scale-[1.05] focus:scale-50"
+            className="btn transition-transform hover:scale-[1.05] focus:scale-[1.05]"
           >
             Register
           </Link>
