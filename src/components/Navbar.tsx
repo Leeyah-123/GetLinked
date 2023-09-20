e7fcb29f5d20b64e74a69ed0147eaec3511980eb
Leeyah-123 <junaidaaliyah260@gmail.com>
2023-09-20 12:22:47 +0100

    Set active navlink

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,8 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 import Logo from './Logo';
 
 const Navbar = () => {
-  const { pathname } = useLocation();
-  const isRegisterRoute = pathname == '/register';
+  const location = useLocation();
 
   return (
     <nav className="py-12  px-5 md:px-10 lg:px-20 flex items-center">
@@ -15,34 +14,45 @@ const Navbar = () => {
         <div className="space-x-32 hidden md:flex items-center">
           <ul className="inline-flex space-x-5 capitalize font-semibold">
             <li>
-              <Link to="/#timeline">timeline</Link>
+              <Link
+                to="/#timeline"
+                className={`${location.hash === '#timeline' && 'active'}`}
+              >
+                timeline
+              </Link>
             </li>
             <li>
-              <Link to="/#overview">overview</Link>
+              <Link
+                to="/#overview"
+                className={`${location.hash === '#overview' && 'active'}`}
+              >
+                overview
+              </Link>
             </li>
             <li>
-              <Link to="/#faqs">faqs</Link>
+              <Link
+                to="/#faqs"
+                className={`${location.hash === '#faqs' && 'active'}`}
+              >
+                faqs
+              </Link>
             </li>
             <li>
-              <Link to="/contact">contact</Link>
+              <NavLink to="/contact">contact</NavLink>
             </li>
           </ul>
 
-          <Link
+          <NavLink
             id="navBtn"
             to="/register"
-            className={`relative ${
-              isRegisterRoute && 'bg-none bg-navy-blue'
-            } btn transition-transform hover:scale-[1.05] focus:scale-[1.05]`}
+            className={({ isActive }) =>
+              `btn relative transition-transform hover:scale-[1.05] focus:scale-[1.05] ${
+                isActive && 'active bg-none bg-navy-blue'
+              }`
+            }
           >
             <span className="relative z-20">Register</span>
-            {isRegisterRoute && (
-              <>
-                <span className="absolute inset-0.5 z-10 bg-navy-blue rounded-md"></span>
-                <span className="absolute inset-0 bg-gradient-to-b from-[#9A39FF] to-[#FF29B9] border-transparent rounded-md"></span>
-              </>
-            )}
-          </Link>
+          </NavLink>
         </div>
 
         <span className="md:hidden border border-slate-400 rounded-md p-2"></span>
