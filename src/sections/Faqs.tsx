951475db58b75c2af429e2388b2c52496ccb99a4
Leeyah-123 <junaidaaliyah260@gmail.com>
2023-09-23 02:21:57 +0100

    Repo Timeline section

--- src/sections/Faqs.tsx
+++ src/sections/Faqs.tsx
@@ -19,30 +19,30 @@ const Faqs = () => {
   return (
     <SectionContainer id="faqs" position="right">
       {/*  */}
-      <div className="grow space-y-5 max-w-xl text-center md:text-left">
-        <h5 className="text-4xl font-semibold">
+      <div className="text-[3.4vw] grow space-y-5 max-w-xl text-center md:text-left lg:text-sm">
+        <h5 className="text-xl font-bold clash-display lg:text-3xl">
           Frequently Asked
           <br />
-          <span className="text-magenta">Questions</span>
+          <span className="text-[var(--color-3)]">Questions</span>
         </h5>
         <p>
           We got answers to the questions that you might want to ask about{' '}
           <span className="font-semibold">getlinked Hackathon 1.0</span>
         </p>
 
-        <div className="">
+        <div>
           {faqs.map(({ answer, question }, index) => (
             <div key={index} className="my-10">
               <button
                 aria-controls={`panel${index + 1}-content`}
                 aria-expanded={index === activeFaq}
-                className="flex w-full justify-between border-b border-magenta pb-3 cursor-pointer"
+                className="flex w-full justify-between border-b border-[var(--color-3)] pb-3 cursor-pointer"
                 onClick={() => handleFaq(index)}
               >
                 <p id={`panel${index + 1}-heading`} className="leading-tight">
                   {question}
                 </p>
-                <span className="text-magenta ml-2" aria-hidden="true">
+                <span className="text-[var(--color-3)] ml-2" aria-hidden="true">
                   {index === activeFaq ? <AiOutlineMinus /> : <AiOutlinePlus />}
                 </span>
               </button>
